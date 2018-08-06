33776209bd2b26155ece79dd63a3039c9cdbfc83
Administrator <Administrator@Yasmeen-PC>
2018-08-06 14:14:33 +0530

    coral

---
+++ CoralFrameWork/src/test/java/abc/MyTest.java
@@ -0,0 +1,14 @@
+package abc;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class MyTest {
+@Test
+public void mycrTest()
+{
+	WebDriver driver=new FirefoxDriver();
+	
+}
+}
